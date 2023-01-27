82341bda758ca31472b25fb3dd2bd7d52773df7d
lakshmikanthan1025 <123475438+lakshmikanthan1025@users.noreply.github.com>
2023-01-27 15:23:41 +0530

    School.java

---
+++ School.java
@@ -0,0 +1,16 @@
+class School{
+static String School_name = "St.Antony's Primary School";
+int mark;private int salary;
+public static void main(String[] args)
+{
+
+}
+void conduct_exams()
+{
+System.out.println();
+}
+void publish_result(int mark){
+this.mark = mark;
+System.out.println(mark);
+}
+}
