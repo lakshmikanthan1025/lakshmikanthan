18c2a593b2a7d5e74d7c78cbbb245b97fb7fece0
lakshmikanthan1025 <123475438+lakshmikanthan1025@users.noreply.github.com>
2023-01-27 15:24:46 +0530

    Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,9 @@
+class Teacher{
+public static void main(String []args){
+School teacher = new School();
+teacher.conduct_exams();
+teacher.publish_result(75);
+System.out.println(School.School_name );
+System.out.println(School.salary);
+}
+}
